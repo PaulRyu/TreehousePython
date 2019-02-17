f227d0d89be83fe06e1aac8a8e6533aceab89a28
PaulRyu <pauliedho@gmail.com>
2019-02-17 09:02:22 -0600

    Create Project File for Introducing Lists.

---
+++ Beginning Python Track/2 - Introducing Lists/Introducing Lists Project/shopping_list.py
@@ -0,0 +1,31 @@
+# ------ Project Requirement 1 ------
+# Create a new empty list named shopping_list.
+shopping_list = []
+
+
+# Create a function named add_to_list that declares a parameter named item
+# Add the item to the list
+def add_to_list(item):
+    shopping_list.append(item)
+
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+    Enter 'DONE' to stop adding items.
+    Enter 'HELP' for this help.
+    """)
+
+
+while True:
+    new_item = input("> ")
+
+    if new_item == "DONE":
+        break
+
+    elif new_item == "HELP":
+        show_help()
+        continue
+
+    # Call add_to_list with new_item as an argument
+    add_to_list(new_item)
